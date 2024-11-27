26cbf938b5e8fb1112327d56cd2c661e585f1162
20galmeida <20galmeida@gmail.com>
2024-11-26 22:26:44 -0300

    backgroud layer fix

--- Assets/PlayerCollision.cs
+++ Assets/PlayerCollision.cs
@@ -5,35 +5,27 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
 
-    [SerializeField] private AudioSource src;
-    [SerializeField] private AudioClip collisionSfx;
+	[SerializeField] private AudioSource src;
+	[SerializeField] private AudioClip collisionSfx;
 
-    private void Start()
-    {
-        GameManager.Instance.onPlay.AddListener(ActivatePlayer);
-    }
+	private void Start()
+	{
+		GameManager.Instance.onPlay.AddListener(ActivatePlayer);
+	}
 
-    private void ActivatePlayer()
-    {
-        gameObject.SetActive(true);
-    }
+	private void ActivatePlayer()
+	{
+		gameObject.SetActive(true);
+	}
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.transform.CompareTag("Obstacle"))
-        {
-            StartCoroutine(HandleCollision());
-        }
-    }
-
-    private IEnumerator HandleCollision()
-    {
-        src.PlayOneShot(collisionSfx, 0.5f);
-
-        yield return new WaitForSeconds(collisionSfx.length);
-
-        gameObject.SetActive(false);
-        GameManager.Instance.GameOver();
-    }
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.transform.tag == "Obstacle")
+		{
+			src.PlayOneShot(collisionSfx, 0.5f);
+			gameObject.SetActive(false);
+			GameManager.Instance.GameOver();
+		}
+	}
 
 }
